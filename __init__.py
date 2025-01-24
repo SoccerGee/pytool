2f9a6b88e997723c8066aa794dbf67142da82e6a
Grant <grant@ternitup.com>
2025-01-24 03:24:13 -0500

    First

---
+++ __init__.py
@@ -0,0 +1,40 @@
+import asyncio
+import sys
+import functools
+from pathlib import Path
+import signal
+
+async def char_generator(filename):
+    dir = Path(__file__).parent
+    with open(dir / filename, 'r') as file:
+        for line in file:
+            for char in line:
+                yield char
+                await asyncio.sleep(.001)
+
+def signal_handler():
+    print("\nReceived SIGINT. Cancelling tasks...")
+    for task in asyncio.all_tasks():
+        task.cancel()
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    transport, _ = await loop.connect_write_pipe(
+        asyncio.Protocol,
+        sys.stdout.buffer
+    )
+
+    loop.add_signal_handler(signal.SIGINT, signal_handler)
+
+    try:
+        args = sys.argv[1:]
+        async for char in char_generator(args[0]):
+            transport.write(char.encode())
+    except asyncio.CancelledError:
+        print("Exiting...")
+    finally:
+        transport.close()
+
+asyncio.run(main())
+
